2db0d053d813006dbd5c9ec02e133f1098a390c7
Sarah Pendhari <97763192+SarahPendhari@users.noreply.github.com>
2022-12-09 21:40:10 +0530

    Add files via upload

---
+++ books webstie/books webstie/js/script.js
@@ -0,0 +1,262 @@
+$(document).ready(function() {
+  var item, tile, author, publisher, bookLink, bookImg;
+  var outputList = document.getElementById("list-output");
+  var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+  var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
+  var placeHldr = '<img src="https://via.placeholder.com/150">';
+  var searchData;
+
+  //listener for search button
+  $("#search").click(function() {
+    outputList.innerHTML = ""; //empty html output
+    document.body.style.backgroundImage = "url('')";
+     searchData = $("#search-box").val();
+     //handling empty search input field
+     if(searchData === "" || searchData === null) {
+       displayError();
+     }
+    else {
+       // console.log(searchData);
+       // $.get("https://www.googleapis.com/books/v1/volumes?q="+searchData, getBookData()});
+       $.ajax({
+          url: bookUrl + searchData,
+          dataType: "json",
+          success: function(response) {
+            console.log(response)
+            if (response.totalItems === 0) {
+              alert("no result!.. try again")
+            }
+            else {
+              $("#title").animate({'margin-top': '5px'}, 1000); //search box animation
+              $(".book-list").css("visibility", "visible");
+              displayResults(response);
+            }
+          },
+          error: function () {
+            alert("Something went wrong.. <br>"+"Try again!");
+          }
+        });
+      }
+      $("#search-box").val(""); //clearn search box
+   });
+
+   /*
+   * function to display result in index.html
+   * @param response
+   */
+   function displayResults(response) {
+      for (var i = 0; i < response.items.length; i+=2) {
+        item = response.items[i];
+        title1 = item.volumeInfo.title;
+        author1 = item.volumeInfo.authors;
+        publisher1 = item.volumeInfo.publisher;
+        bookLink1 = item.volumeInfo.previewLink;
+        bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
+        bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
+
+        item2 = response.items[i+1];
+        title2 = item2.volumeInfo.title;
+        author2 = item2.volumeInfo.authors;
+        publisher2 = item2.volumeInfo.publisher;
+        bookLink2 = item2.volumeInfo.previewLink;
+        bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
+        bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
+
+        // in production code, item.text should have the HTML entities escaped.
+        outputList.innerHTML += '<div class="row mt-4">' +
+                                formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
+                                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                                '</div>';
+
+        console.log(outputList);
+      }
+   }
+
+   /*
+   * card element formatter using es6 backticks and templates (indivial card)
+   * @param bookImg title author publisher bookLink
+   * @return htmlCard
+   */
+   function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+     // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
+     var viewUrl = 'book.html?isbn='+bookIsbn; //constructing link for bookviewer
+     var htmlCard = `<div class="col-lg-6">
+       <div class="card" style="">
+         <div class="row no-gutters">
+           <div class="col-md-4">
+             <img src="${bookImg}" class="card-img" alt="...">
+           </div>
+           <div class="col-md-8">
+             <div class="card-body">
+               <h5 class="card-title">${title}</h5>
+               <p class="card-text">Author: ${author}</p>
+               <p class="card-text">Publisher: ${publisher}</p>
+               <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>`
+     return htmlCard;
+   }
+
+  
+});
+
+
+
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  searchForm.classList.remove('active');
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+}
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+  fadeOut();
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+  setTimeout(loader, 4000);
+}
+
+var swiper = new Swiper(".books-slider", {
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".featured-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
